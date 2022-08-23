8724ffd80b0a5da436e51caadb74c2d471d96f62
hbadejo <badejohakeem95@gmail.com>
2022-08-23 22:51:58 +0100

    Readme and solutions for cond. loops. and function

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_number = abs(number) % 10
+if number < 0:
+    last_number = -last_number
+
+print(f"Last digit of {number} is {last_number}", end=" ")
+if last_number > 5:
+    print("and is greater than 5")
+elif last_number == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for _ in range(97, 123):
+    if _ == 101 or _ == 113:
+        continue
+    print(chr(_), end="")

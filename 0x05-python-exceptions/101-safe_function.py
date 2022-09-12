d7158788fcf7d9187b03c6c103f12a63823e31d5
hbadejo <badejohakeem95@gmail.com>
2022-09-13 00:03:37 +0100

    Pycodestyle error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
 
     try:
         result = fct(*args)
-        return (result)
-    except:
+        return result
+    except (TypeError, ZeroDivisionError, IndexError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return None

c4261e1559e588476f95d023646020464cd3c9f8
hbadejo <badejohakeem95@gmail.com>
2022-09-17 23:09:18 +0100

    Solution to assignment: 3-say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A function that prints My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function takes two args <string>
+    Return My name is arg[0] arg[1]
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))

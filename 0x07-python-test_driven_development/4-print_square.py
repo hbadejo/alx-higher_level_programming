95cdf98a76e3c4f3e1c51cd6807979f06a9cb028
hbadejo <badejohakeem95@gmail.com>
2022-09-18 00:14:14 +0100

    Solution to assignment: 4-print_square.txt

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+A function that prints a square with the character #.
+"""
+
+
+def print_square(size):
+    """
+    Function return a square of pounds sign
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 1:
+        print("#")
+
+    if size > 1:
+        for i in range(size):
+            for _ in range(size):
+                print("#", end="")
+            print("")

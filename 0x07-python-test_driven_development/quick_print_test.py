ee8079b2fe8cc3d13199bd3884ae2acf6eede712
hbadejo <badejohakeem95@gmail.com>
2022-09-18 11:04:49 +0100

    Print test

---
+++ 0x07-python-test_driven_development/quick_print_test.py
@@ -0,0 +1,20 @@
+
+print("Woah now.", end="")
+print("\n")
+print("This is messy.")
+"""
+OUTPUT
+======
+Woah now.
+
+This is messy.
+"""
+
+
+print("Woah now.", end="")
+print("This is messy.")
+"""
+OUTPUT
+======
+Woah now.This is messy.
+"""

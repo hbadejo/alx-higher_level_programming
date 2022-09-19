575e0e635d0466d85403abbb74213f671df3d8c8
hbadejo <badejohakeem95@gmail.com>
2022-09-19 23:05:01 +0100

    2-rectangle initial

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+An empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class to get the size of a rectangle
+
+    Class takes two arguments: heightand width
+        of rectangle. Arguments must be of type int
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """Retrieve private instance attribute"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set private instance attribute
+        Raise a TypeError if value is not int
+        Raise a ValueError if value is < 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    @property
+    def width(self):
+        """Retrieve private instance attribute"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set private instance attribute
+        Raise a TypeError if value is not int
+        Raise a ValueError if value is < 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    def area(self):
+        """Computes the Area of rectangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """
+        Returns The perimeter of the rectangle
+        if any of the values turns equals 0,
+        the computation equals 0
+        """
+        if 0 in [self.height, self.width]:
+            return 0
+        else:
+            return 2 * (self.height + self.width)

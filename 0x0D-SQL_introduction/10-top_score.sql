fe60d0ccde8cf7da50ce724da3ef7eb5aa82662d
hbadejo <badejohakeem95@gmail.com>
2022-11-02 04:20:21 +0100

    Table creation and data extraction

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists the score and name records from the table second_table.
+-- Records are ordered with score in descending format.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;

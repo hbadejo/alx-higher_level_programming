3d30cb09d48acddd09bdecd612661cfe534dd998
hbadejo <badejohakeem95@gmail.com>
2022-11-29 19:36:57 +0100

    Using array to print

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-arr.forEach((value) => console.log(value));
+for (let line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
+}
